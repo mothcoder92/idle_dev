55f6117d1a209f4780f3d47ca8d853bf7a19079b
Dominik Raffelsberg <raffelsberg.d19@htlwienwest.at>
2025-11-23 18:18:18 +0100

    Test Commit Dominik

--- idleDevGame/src/main/java/at/ac/hcw/idledevgame/Launcher.java
+++ idleDevGame/src/main/java/at/ac/hcw/idledevgame/Launcher.java
@@ -3,6 +3,7 @@ package at.ac.hcw.idledevgame;
 import javafx.application.Application;
 
 public class Launcher {
+    // Das ist ein Test für einen Commit
     public static void main(String[] args) {
         Application.launch(GameApplication.class, args);
     }
