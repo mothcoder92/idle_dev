dae7105158c6fd70ac3749df1c6c26e7166ce50f
Dominik Raffelsberg <dominik.raffelsberg@sozialbau.at>
2025-12-05 10:27:09 +0100

    first steps javaFX

--- src/main/java/at/ac/hcw/idledevgame/GameApplication.java
+++ src/main/java/at/ac/hcw/idledevgame/GameApplication.java
@@ -11,8 +11,8 @@ public class GameApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(GameApplication.class.getResource("main-game-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
-        stage.setTitle("Idle Developer");
+        Scene scene = new Scene(fxmlLoader.load(), 1000, 562);
+        stage.setTitle("Idle Game");
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/at/ac/hcw/idledevgame/GameController.java
+++ src/main/java/at/ac/hcw/idledevgame/GameController.java
@@ -5,10 +5,22 @@ import javafx.scene.control.Label;
 
 public class GameController {
     @FXML
-    private Label welcomeText;
+    private Label levelCodingSpeed;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Label levelQualityOfCode;
+
+    @FXML
+    protected void upgradeCodingSpeed() {
+        int lvl =  Integer.parseInt(levelCodingSpeed.getText().substring(6));
+        lvl++;
+        levelCodingSpeed.setText("Stufe " + lvl);
+    }
+
+    @FXML
+    protected void upgradeQualityOfCode() {
+        int lvl =  Integer.parseInt(levelQualityOfCode.getText().substring(6));
+        lvl++;
+        levelQualityOfCode.setText("Stufe " + lvl);
     }
 }
