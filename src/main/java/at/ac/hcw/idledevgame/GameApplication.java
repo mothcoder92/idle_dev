82303d25603077c8560afb0df816fd282749236a
Dominik Raffelsberg <raffelsberg.d19@htlwienwest.at>
2026-01-16 16:35:27 +0100

    ratio fix

--- src/main/java/at/ac/hcw/idledevgame/GameApplication.java
+++ src/main/java/at/ac/hcw/idledevgame/GameApplication.java
@@ -11,10 +11,10 @@ public class GameApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(GameApplication.class.getResource("main-game-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1000, 562);
+        Scene scene = new Scene(fxmlLoader.load(), 1010, 572);
         stage.setTitle("Idle Macrop");
-        stage.setMaxWidth(1000);
-        stage.setMaxHeight(562);
+        stage.setMaxWidth(1010);
+        stage.setMaxHeight(572);
         stage.setScene(scene);
         stage.show();
     }
