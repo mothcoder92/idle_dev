fd2dcbef6e3c66603c11ea3c76756b4577e3db47
ELGAHSH <mahmod.el@hotmail.com>
2026-01-16 17:16:55 +0100

    pay salaries methode

--- src/main/java/classes/Game.java
+++ src/main/java/classes/Game.java
@@ -3,6 +3,7 @@ package classes;
 import at.ac.hcw.idledevgame.Launcher;
 import javafx.beans.property.*;
 import javafx.collections.FXCollections;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -52,16 +53,17 @@ public class Game {
      * Default constructor, with test-settings
      * @deprecated used for console-game and testing
      */
-    public Game(){
+    public Game() {
         logStartingValues();
     }
 
     /**
      * Custom game constructor
-     * @param companyName //the chosen company name
-     * @param startingCapital   //the starting capital
+     *
+     * @param companyName     //the chosen company name
+     * @param startingCapital //the starting capital
      */
-    public Game(String companyName, int startingCapital){
+    public Game(String companyName, int startingCapital) {
         this.companyName.set(companyName);
         this.currentCapital.set(startingCapital);
         logStartingValues();
@@ -74,9 +76,10 @@ public class Game {
 
     /**
      * A console-based implementation for testing purposes
+     *
      * @deprecated used for development and testing
      */
-    public void consoleGame(){
+    public void consoleGame() {
         List<String> commands = new ArrayList<>();
         commands.add("1. Progress to next day");
         commands.add("2. Work on Contracts (success randomized)");
@@ -91,37 +94,37 @@ public class Game {
 
         boolean consoleGame = true;
         int counter = 0;
-        int rnd = new Random().nextInt(50,200);
+        int rnd = new Random().nextInt(50, 200);
 
-        while(consoleGame){
+        while (consoleGame) {
             commands.forEach(System.out::println);
             String input = System.console().readLine();
-            switch (input){
+            switch (input) {
                 case "1":
                     this.nextDay();
                     break;
-                    case "2":
-                        progressContract();
-                        progressContract();
-                        break;
-                        case "3":
-                            this.developers.add(hireDeveloper(5));
-                            System.out.println("Developer has been successfully hired!");
-                            break;
-                            case "4":
-                                resetGame();
-                                System.out.println("Game has been reset.");
-                                break;
-                                case "5":
-                                    printCurrentGameStats();
-                                    break;
-                                    case "6":
-                                        printGameLog();
-                                        break;
-                                        case "7":
-                                            System.exit(0);
-                                            case "8":
-                                                Launcher.main(new String[]{});
+                case "2":
+                    progressContract();
+                    progressContract();
+                    break;
+                case "3":
+                    this.developers.add(hireDeveloper(5));
+                    System.out.println("Developer has been successfully hired!");
+                    break;
+                case "4":
+                    resetGame();
+                    System.out.println("Game has been reset.");
+                    break;
+                case "5":
+                    printCurrentGameStats();
+                    break;
+                case "6":
+                    printGameLog();
+                    break;
+                case "7":
+                    System.exit(0);
+                case "8":
+                    Launcher.main(new String[]{});
                 default:
                     System.out.println("-------------");
                     System.out.println("Invalid input");
@@ -133,21 +136,22 @@ public class Game {
     /**
      * Auto-playthrough with randomized values
      * for testing purposes
+     *
+     * @return //false if errors occurred
      * @deprecated used for development and testing
-     * @return  //false if errors occurred
      */
-    public boolean gameAutoRun(){
+    public boolean gameAutoRun() {
         try {
             this.running = true;
             int timer = 0;
-            int rnd = new Random().nextInt(0,1000);
-            while(running && timer <= 10000) {
+            int rnd = new Random().nextInt(0, 1000);
+            while (running && timer <= 10000) {
                 this.nextDay();
                 this.progressContract();
                 this.progressContract();
                 this.wait(rnd);
-                timer+= rnd;
-                rnd = new Random().nextInt(0,1000);
+                timer += rnd;
+                rnd = new Random().nextInt(0, 1000);
             }
             return true;
         } catch (Exception e) {
@@ -160,13 +164,13 @@ public class Game {
      * Timeline management, continually calls
      * Minute->Hour->Day
      */
-    public void advanceMinute(){
+    public void advanceMinute() {
         this.minute++;
         int contractProgress = 0;
 
-        for(Developer developer : this.developers){
+        for (Developer developer : this.developers) {
             developer.addProgress();
-            if(developer.getProgress().get() >= 1.0){
+            if (developer.getProgress().get() >= 1.0) {
                 developer.setProgress(0.0);
                 contractProgress += developer.work(currentContract.get().getContractDifficulty());
                 //numberofLines
@@ -175,63 +179,75 @@ public class Game {
 
         this.currentContract.get().progressContract(contractProgress);
 
-        if(minute >= MINUTES_PER_HOUR){
+        if (minute >= MINUTES_PER_HOUR) {
             hour++;
             minute = 0;
             advanceHour();
         }
     }
 
-    public void advanceHour(){
+    public void advanceHour() {
 
         //per hour game object things
 
-        if(hour >= WORK_HOURS_PER_DAY){
+        if (hour >= WORK_HOURS_PER_DAY) {
             hour = 0;
             advanceDay();
         }
     }
 
-    public void advanceDay(){
-        currentDay.set(currentDay.get()+1);
+    public void advanceDay() {
+        paySalaries();
+
+        currentDay.set(currentDay.get() + 1);
         //todo: events
     }
 
-    public void addTotalContractsCompleted(){
+    public void paySalaries() {
+        int totalSalaries = 0;
 
-        this.totalContractsCompleted.set(totalContractsCompleted.get()+1);
+        for (Developer dev : this.developers) {
+            totalSalaries += dev.getSalary();
+        }
+
+        this.currentCapital.set(this.currentCapital.get() - totalSalaries);
     }
 
-    public void getNewContract(){
-        if(totalContractsCompleted.get() <= 3){
+    public void addTotalContractsCompleted() {
+
+        this.totalContractsCompleted.set(totalContractsCompleted.get() + 1);
+    }
+
+    public void getNewContract() {
+        if (totalContractsCompleted.get() <= 3) {
             this.currentContract.set(new Contract(1));
-        }
-        else if(totalContractsCompleted.get() <= 3 && totalContractsCompleted.get() <= 6){
+        } else if (totalContractsCompleted.get() <= 3 && totalContractsCompleted.get() <= 6) {
             this.currentContract.set(new Contract(2));
-        }
-        else{
+        } else {
             this.currentContract.set(new Contract(3));
         }
     }
 
     //region Game control
+
     /**
      * Pause the game for specified time
+     *
      * @param milliseconds //time to pause
      */
-    private void wait(int milliseconds){
+    private void wait(int milliseconds) {
         try {
-            logGameAction("Sleeping for "+milliseconds+" milliseconds");
+            logGameAction("Sleeping for " + milliseconds + " milliseconds");
             Thread.sleep(milliseconds);
         } catch (InterruptedException e) {
             errors.add(e);
         }
     }
 
-    public boolean progressContract (){
+    public boolean progressContract() {
         try {
             int codeLinesWritten = 0;
-            for(Developer dev : this.developers){
+            for (Developer dev : this.developers) {
                 codeLinesWritten += dev.work(currentContract.get().getContractDifficulty()); //todo: check contract difficulty
             }
 
@@ -240,10 +256,10 @@ public class Game {
 
 
             //test values
-            int rnd = new Random().nextInt(0,4);
-            if(rnd == 0){
+            int rnd = new Random().nextInt(0, 4);
+            if (rnd == 0) {
                 logGameAction("Developers keep working on the current Contract.");
-            } else if(rnd == 1){
+            } else if (rnd == 1) {
                 logGameAction("Contract almost complete");
             } else {
                 logGameAction("Contract has been successfully finished!");
@@ -259,30 +275,32 @@ public class Game {
         }
     }
 
-    public void nextDay(){
+    public void nextDay() {
         this.currentDay.add(1);
         logGameAction("has started.");
     }
 
     /**
      * Hire a new developer with max ranks
+     *
      * @param upperRankBound max possible ranks
      * @return Developer object
      */
-    public Developer hireDeveloper(int upperRankBound){
+    public Developer hireDeveloper(int upperRankBound) {
         Developer dev = new Developer(upperRankBound);
-        this.gameLog.add("Hired "+dev.getName()+".");
+        this.gameLog.add("Hired " + dev.getName() + ".");
         return dev;
     }
 
     /**
      * Adds three developers to the list of
      * potential developers.
+     *
      * @param upperRankBound maximum initial skill level
      */
-    public void hireNewDevelopers(int upperRankBound){
+    public void hireNewDevelopers(int upperRankBound) {
         this.potentialDevelopers.clear();
-        for(int i = 0; i < 3; i++){
+        for (int i = 0; i < 3; i++) {
             potentialDevelopers.add(new Developer(upperRankBound));
         }
     }
@@ -290,41 +308,41 @@ public class Game {
     /**
      * Resets the game to starting values
      */
-    private void resetGame(){
+    private void resetGame() {
         setCurrentCapital(startingCapital.getValue());
         this.currentDay.set(1);
         this.gameLog.clear();
         this.developers.clear();
     }
 
-    private void logStartingValues(){
-        this.gameLog.add("New game started as "+this.companyName);
-        this.gameLog.add("Starting funds: "+this.startingCapital);
+    private void logStartingValues() {
+        this.gameLog.add("New game started as " + this.companyName);
+        this.gameLog.add("Starting funds: " + this.startingCapital);
         //todo: implement
     }
 
-    private void logEndingValues(){
-        this.gameLog.add("Ending funds: "+this.currentCapital);
-        this.gameLog.add("Final day: "+this.currentDay);
+    private void logEndingValues() {
+        this.gameLog.add("Ending funds: " + this.currentCapital);
+        this.gameLog.add("Final day: " + this.currentDay);
     }
 
     /***
      * Add log information to logging List
      * @param action the thing that happened
      */
-    private void logGameAction(String action){
+    private void logGameAction(String action) {
         this.gameLog.add("Day " + currentDay + ": " + action);
     }
 
     /**
      * Print internal log of game-actions to console
      */
-    public void printGameLog(){
+    public void printGameLog() {
         logEndingValues();
         System.out.println("-------------------------------------------------");
-        if(errors.isEmpty()){
+        if (errors.isEmpty()) {
             gameLog.forEach(System.out::println);
-        }else {
+        } else {
             errors.forEach(System.out::println);
         }
         System.out.println("-------------------------------------------------");
@@ -333,23 +351,23 @@ public class Game {
     /**
      * Print current game stats to console
      */
-    public void printCurrentGameStats(){
+    public void printCurrentGameStats() {
         System.out.println("#############################################");
         System.out.println("Currently Playing as: " + this.companyName);
         System.out.println("Current Capital: " + this.currentCapital);
         System.out.println("Current Day: " + this.currentDay);
         System.out.println("Workforce: ");
-        developers.forEach((d)-> System.out.println(d.getName()+", Salary: "+ d.getSalary()));
+        developers.forEach((d) -> System.out.println(d.getName() + ", Salary: " + d.getSalary()));
         System.out.println("#############################################");
     }
     //endregion
 
     //region ViewModel Properties
-    public StringProperty companyNameProperty(){
+    public StringProperty companyNameProperty() {
         return this.companyName;
     }
 
-    public IntegerProperty currentCapitalProperty(){
+    public IntegerProperty currentCapitalProperty() {
         return this.currentCapital;
     }
 
@@ -357,20 +375,64 @@ public class Game {
 
 
     //region getter & setter
-    public String getCompanyName() {return this.companyName.get();}
-    public void setCompanyName(String companyName) {this.companyName.set(companyName);}
-    public int getStartingCapital() {return this.startingCapital.get();}
-    public void setStartingCapital(int startingCapital) {this.startingCapital.set(startingCapital);}
-    public Department getFirstOffice() {return firstOffice;}
-    public void setFirstOffice(Department firstOffice) {this.firstOffice = firstOffice;}
-    public Contract getCurrentContract() {return currentContract.get();}
-    public void setCurrentContract(Contract currentContract) {this.currentContract.set(currentContract);}
-    public int getCurrentCapital() {return this.currentCapital.get();}
-    public void setCurrentCapital(int currentCapital) {this.currentCapital.set(currentCapital);}
-    public int getCurrentDay() {return this.currentDay.get();}
-    public void setCurrentDay(int currentDay) {this.currentDay.set(currentDay);}
-    public int getHour() {return hour;}
-    public void setHour(int hour) {this.hour = hour;}
-    public ObjectProperty<Contract> getContractProperty() {return this.currentContract;}
+    public String getCompanyName() {
+        return this.companyName.get();
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName.set(companyName);
+    }
+
+    public int getStartingCapital() {
+        return this.startingCapital.get();
+    }
+
+    public void setStartingCapital(int startingCapital) {
+        this.startingCapital.set(startingCapital);
+    }
+
+    public Department getFirstOffice() {
+        return firstOffice;
+    }
+
+    public void setFirstOffice(Department firstOffice) {
+        this.firstOffice = firstOffice;
+    }
+
+    public Contract getCurrentContract() {
+        return currentContract.get();
+    }
+
+    public void setCurrentContract(Contract currentContract) {
+        this.currentContract.set(currentContract);
+    }
+
+    public int getCurrentCapital() {
+        return this.currentCapital.get();
+    }
+
+    public void setCurrentCapital(int currentCapital) {
+        this.currentCapital.set(currentCapital);
+    }
+
+    public int getCurrentDay() {
+        return this.currentDay.get();
+    }
+
+    public void setCurrentDay(int currentDay) {
+        this.currentDay.set(currentDay);
+    }
+
+    public int getHour() {
+        return hour;
+    }
+
+    public void setHour(int hour) {
+        this.hour = hour;
+    }
+
+    public ObjectProperty<Contract> getContractProperty() {
+        return this.currentContract;
+    }
     //endregion
 }
